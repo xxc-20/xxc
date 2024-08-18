3d1e6b8f5f844a398801f8c43c688aab4b85cdaf
xxc-20 <2154364559@qq.com>
2024-08-18 14:48:42 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,84 +4,33 @@
 using namespace cv;
 using namespace std;
 
-
 int main(int argc, char *argv[])
 {
 
-    Mat img=imread("d:/pic/image.jpg");
-    Mat backImg=imread("d:/pic/back.jpg");
-    imshow("01",img);
+    Mat frame;
+    Mat backImg;
+    VideoCapture vid(0);
 
-    //1.转hsv
-    Mat hsv;
-    cvtColor(img,hsv,COLOR_BGR2HSV);
+    while(vid.read(frame)){
+        backImg=imread("D:/pic/back04.png"); //背景图片
+        imshow("test01",frame);
+        //imshow("back",backImg);
+        //1.转hsv
+        Mat hsv;
+        cvtColor(frame,hsv,COLOR_BGR2HSV);
 
-    //2.提取红色
-    Mat mark;
-    inRange(hsv,Scalar(0,100,180),Scalar(10,255,255),mark);
-    imshow("2",mark);
-    Mat mark1;
-    inRange(hsv,Scalar(156,120,130),Scalar(180,255,255),mark1);
-    imshow("3",mark1);
-    Mat finMark=mark+mark1;
-    imshow("4",finMark);
+        //2.抠图
+        Mat mask;
+        inRange(hsv,Scalar(100,43,46),Scalar(124,255,255),mask);//保留蓝色区域，但实际想用背景图覆盖蓝色区域
+        //所以3.取反
+        Mat mask1;
+        bitwise_not(mask,mask1);
+        //4.用原图片调用copyTo实现对模板的覆盖
+        frame.copyTo(backImg,mask1);
+        imshow("finalImg",backImg);
 
-    //3.取反
-    Mat finMark1;
-    bitwise_not(finMark,finMark);//只有人物在
-    imshow("test08",finMark);
+        waitKey(50);
+    }
 
-    img.copyTo(backImg,finMark);
-    imshow("5",backImg);
-
-    //img.copyTo(backImg,finMark1);
-    //imshow("6",backImg);
-
-
-
-
-
-
-    /*Mat img=imread("d:/image2.png");
-    imshow("test05",img);
-
-    Mat hsv;
-    cvtColor(img,hsv,COLOR_BGR2HSV);
-    imshow("test06",hsv);
-
-    Mat mask;
-    inRange(hsv,Scalar(100,43,46),Scalar(124,255,255),mask);
-    imshow("test07",mask);
-
-    //取反
-    bitwise_not(mask,mask);
-    imshow("test08",mask);
-
-    Mat redBack =Mat::zeros(img.size(),img.type());
-    redBack=Scalar(40,40,200);
-    imshow("test09",redBack);
-
-    img.copyTo(redBack,mask);
-    imshow("test10",redBack);*/
-
-
-
-   /*Mat img = imread("d:/image1.jpg");
-   imshow("test01",img);
-
-   Mat blurImg;
-   blur(img,blurImg,Size(50,50));
-   imshow("test02",blurImg);
-
-   Mat grayImg;
-   cvtColor(img,grayImg,COLOR_BGR2GRAY);
-   imshow("test03",grayImg);
-
-   //转化为二值化图片
-   Mat threImg;
-   threshold(grayImg,threImg,100,255,THRESH_BINARY);
-   imshow("test04",threImg);*/
-
-   waitKey(0);
-   return 0;
+    return 0;
 }
